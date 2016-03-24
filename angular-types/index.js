8e0289c73f958bf2601a4f45169354daba364c78
Arthur Flachs <arthur.flachs@gmail.com>
2016-03-24 18:51:51 +0100

    Adds type AngularModuleDefinition

--- angular-types/index.js
+++ angular-types/index.js
@@ -8,9 +8,15 @@ export function isAngularModuleCallee(p) {
 
 export function isAngularModuleGetter(p) {
   return p.type === 'CallExpression' && isAngularModuleCallee(p.callee)
-  && p.arguments.length === 1;
+    && p.arguments.length === 1;
 }
 
+export function isAngularModuleSetter(p) {
+  return p.type === 'CallExpression' && isAngularModuleCallee(p.callee)
+    && p.arguments.length === 2;
+}
+
+export const isAngularModuleDefinition = p => isAngularModuleSetter(p);
 export const isAngularModule = p => isAngularModuleGetter(p);
 
 export function isAngularDependencyDefinition(p) {
