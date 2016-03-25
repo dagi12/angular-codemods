35fc26598f928ab91e3af9c38a8c37dd485783bd
Arthur Flachs <arthur.flachs@gmail.com>
2016-03-25 08:42:46 +0100

    Adds method angularDependencyDefinitions for recursively adding the dependencies.

--- angular-module-exports/transformer2.js
+++ angular-module-exports/transformer2.js
@@ -4,15 +4,31 @@ export default function transformer(file, api) {
   const j = api.jscodeshift;
   const {expression, statement, statements} = j.template;
 
+  function angularDependencyDefinition(parentCall, { type, name, path }) {
+    return j.callExpression(j.memberExpression(parentCall, j.identifier(type)), [
+      j.literal(name),
+      j.callExpression(j.identifier('require'), [j.literal(path)]),
+    ]);
+  }
+
+  function angularDependencyDefinitions(parentCall, [definition, ...rest]) {
+    if (!definition) {
+      return parentCall;
+    }
+
+    return angularDependencyDefinitions(angularDependencyDefinition(parentCall, definition), rest);
+  }
+
   return j(file.source)
   .find(j.CallExpression, isAngularModuleDefinition)
   .replaceWith(p => {
-    var parentCall = j(p).get().value;
+    const parentCall = j(p).get().value;
+    const definitions = [
+      { module: 'test_module', type: 'directive', name: 'test_directive', path: '/test/directive.js' },
+      { module: 'test_module', type: 'factory', name: 'test_factory', path: '/test/factory.js' },
+    ];
 
-    return j.callExpression(j.memberExpression(parentCall, j.identifier('directive')), [
-      j.literal('test'),
-      j.callExpression(j.identifier('require'), [j.literal('path')])
-    ]);
+    return angularDependencyDefinitions(parentCall, definitions);
   })
   .toSource({ wrapColumn: true });
 };
