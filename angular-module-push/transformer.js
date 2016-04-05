942cbeb77f6cc2a99976659971d40f90d282cb77
Arthur Flachs <arthur.flachs@gmail.com>
2016-04-05 08:22:54 +0200

    Write exported modules to file (to replace ANGULAR_MODULES global injection)

--- angular-module-push/transformer.js
+++ angular-module-push/transformer.js
@@ -1,3 +1,6 @@
+import path from 'path';
+import fs from 'fs';
+
 export default function transformer(file, api) {
   const j = api.jscodeshift;
   const {expression, statement, statements} = j.template;
@@ -19,6 +22,25 @@ export default function transformer(file, api) {
     },
   })
   .replaceWith(p => {
+    const destination = path.resolve('test', 'modules.js');
+
+    var previous;
+    try {
+      previous = fs.readFileSync(destination, { flags: 'a+', encoding: 'utf8' });
+    } catch (e) {
+      previous = 'module.exports = [\n];';
+    }
+    const breakdown = previous.split('\n');
+    const require = `require('${path.resolve(file.path)}'),`
+    const nextBreakdown = breakdown.slice(0, -1).concat(require, breakdown.slice(-1));
+    console.log(nextBreakdown);
+
+    try {
+      fs.writeFileSync(destination, nextBreakdown.join('\n'));
+    } catch (e) {
+      console.error(e);
+    }
+
     return j.assignmentExpression(
       '=',
       j.memberExpression(j.identifier('module'), j.identifier('exports')),
